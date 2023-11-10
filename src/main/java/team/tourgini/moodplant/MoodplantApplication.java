7806dd562f49e7fe628a94f0f7506b9fc85f5232
Daeeun Ko <7273322@naver.com>
2023-11-10 10:21:05 +0900

    chore: init project

---
+++ src/main/java/team/tourgini/moodplant/MoodplantApplication.java
@@ -0,0 +1,13 @@
+package team.tourgini.moodplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodplantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodplantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/tourgini/moodplant/MoodplantApplicationTests.java
@@ -0,0 +1,13 @@
+package team.tourgini.moodplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodplantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
